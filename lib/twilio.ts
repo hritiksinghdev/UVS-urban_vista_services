389f7dfbdc086201f0bc59a94a1acfbdfa52b2f0
hritik2006singh-wq <hritik.2006.singh@gmail.com>
2026-03-15 21:35:43 +0530

    Fix Content Security Policy crashing React Three Fiber and Firebase WebSockets in production

--- lib/twilio.ts
+++ lib/twilio.ts
@@ -1,7 +1,7 @@
 import twilio from 'twilio'
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID!
-const authToken = process.env.TWILIO_AUTH_TOKEN!
+const accountSid = process.env.TWILIO_ACCOUNT_SID || 'AC_dummy_sid'
+const authToken = process.env.TWILIO_AUTH_TOKEN || 'dummy_token'
 
 export const twilioClient = twilio(accountSid, authToken)
 export const TWILIO_PHONE = process.env.TWILIO_PHONE_NUMBER!
